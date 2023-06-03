4645a6ccc1a734e6b2f023141334e75e0a03cc02
YuRi Chung <68888172+erica00j@users.noreply.github.com>
2023-06-04 04:57:51 +0900

    Update Form_Home.java

--- hotel-project/src/view/form/Form_Home.java
+++ hotel-project/src/view/form/Form_Home.java
@@ -1,6 +1,7 @@
 package view.form;
 
 import javax.swing.ImageIcon;
+import java.awt.image.*;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import view.model.StatusType;
@@ -10,122 +11,135 @@ import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import view.model.StatusType;
-import view.swing.ScrollBar;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.GroupLayout;
 import java.awt.GridLayout;
+import java.awt.Image;
+
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.Graphics;
+
 import javax.swing.SwingConstants;
 import javax.swing.JScrollBar;
 import javax.swing.JToolBar;
 import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
 
-public class Form_Home extends JPanel {
+public class Form_Home extends javax.swing.JPanel {
+	
+	
+    public Form_Home() {
+        initComponents();
+        JPanel p = new JPanel();
+        p.setBackground(Color.WHITE);   
+        
+        }
 
-	 public Form_Home() {
-	        initComponents();
-	        JPanel p = new JPanel();
-	        p.setBackground(Color.WHITE);   
-	        }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
 
-	    @SuppressWarnings("unchecked")
-	    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-	    private void initComponents() {
-
-	    	panel = new javax.swing.JLayeredPane();
-	        panelBorder1 = new view.swing.PanelBorder();
-	        jLabel1 = new javax.swing.JLabel();
-	        
-	        setBackground(new java.awt.Color(242, 242, 242));
-	        
-	        panel.setLayout(new java.awt.GridLayout(1, 0, 10, 0));
-	        
-	        panelBorder1.setBackground(new java.awt.Color(255, 255, 255));
-
-	        jLabel1.setFont(new java.awt.Font("sansserif", 1, 18)); // NOI18N
-	        jLabel1.setForeground(new java.awt.Color(127, 127, 127));
-	        jLabel1.setText("Rooms");
-	        
-	        JButton btnNewButton_1 = new JButton("New button");
-	        
-	        btnNewButton = new JButton("New button");
-	        
-	        JScrollBar scrollBar = new JScrollBar();
-	        
-	        JButton btnNewButton_1_1 = new JButton("New button");
-	        
-	        JButton btnNewButton_1_2 = new JButton("New button");
-	        
-	        
-	        javax.swing.GroupLayout panelBorder1Layout = new javax.swing.GroupLayout(panelBorder1);
-	        panelBorder1Layout.setHorizontalGroup(
-	        	panelBorder1Layout.createParallelGroup(Alignment.LEADING)
-	        		.addGroup(panelBorder1Layout.createSequentialGroup()
-	        			.addGap(20)
-	        			.addGroup(panelBorder1Layout.createParallelGroup(Alignment.LEADING)
-	        				.addGroup(panelBorder1Layout.createSequentialGroup()
-	        					.addGroup(panelBorder1Layout.createParallelGroup(Alignment.LEADING, false)
-	        						.addGroup(panelBorder1Layout.createSequentialGroup()
-	        							.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 406, GroupLayout.PREFERRED_SIZE)
-	        							.addPreferredGap(ComponentPlacement.UNRELATED)
-	        							.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 406, GroupLayout.PREFERRED_SIZE))
-	        						.addGroup(panelBorder1Layout.createSequentialGroup()
-	        							.addComponent(btnNewButton_1_1, GroupLayout.PREFERRED_SIZE, 406, GroupLayout.PREFERRED_SIZE)
-	        							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-	        							.addComponent(btnNewButton_1_2, GroupLayout.PREFERRED_SIZE, 406, GroupLayout.PREFERRED_SIZE)))
-	        					.addPreferredGap(ComponentPlacement.RELATED)
-	        					.addComponent(scrollBar, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-	        				.addComponent(jLabel1))
-	        			.addContainerGap())
-	        );
-	        panelBorder1Layout.setVerticalGroup(
-	        	panelBorder1Layout.createParallelGroup(Alignment.LEADING)
-	        		.addGroup(panelBorder1Layout.createSequentialGroup()
-	        			.addGap(20)
-	        			.addComponent(jLabel1)
-	        			.addPreferredGap(ComponentPlacement.RELATED)
-	        			.addGroup(panelBorder1Layout.createParallelGroup(Alignment.LEADING, false)
-	        				.addComponent(scrollBar, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-	        				.addGroup(panelBorder1Layout.createSequentialGroup()
-	        					.addGroup(panelBorder1Layout.createParallelGroup(Alignment.TRAILING, false)
-	        						.addComponent(btnNewButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-	        						.addComponent(btnNewButton_1, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 236, Short.MAX_VALUE))
-	        					.addPreferredGap(ComponentPlacement.RELATED)
-	        					.addGroup(panelBorder1Layout.createParallelGroup(Alignment.LEADING)
-	        						.addComponent(btnNewButton_1_1, GroupLayout.PREFERRED_SIZE, 236, GroupLayout.PREFERRED_SIZE)
-	        						.addComponent(btnNewButton_1_2, GroupLayout.PREFERRED_SIZE, 236, GroupLayout.PREFERRED_SIZE))
-	        					.addContainerGap())))
-	        );
-	        panelBorder1.setLayout(panelBorder1Layout);
-	        
-	        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-	        this.setLayout(layout);
-	        layout.setHorizontalGroup(
-	            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-	            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-	                .addGap(20, 20, 20)
-	                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-	                    .addComponent(panelBorder1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-	                    .addComponent(panel, javax.swing.GroupLayout.DEFAULT_SIZE, 875, Short.MAX_VALUE))
-	                .addGap(20, 20, 20))
-	        );
-	        layout.setVerticalGroup(
-	            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-	            .addGroup(layout.createSequentialGroup()
-	                .addGap(20, 20, 20)
-	                .addComponent(panel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-	                .addGap(20, 20, 20)
-	                .addComponent(panelBorder1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-	                .addGap(20, 20, 20))
-	        );
-	    }// </editor-fold>//GEN-END:initComponents
-
-
-	    // Variables declaration - do not modify//GEN-BEGIN:variables
-	    private javax.swing.JLabel jLabel1;
-	    private javax.swing.JLayeredPane panel;
-	    private view.swing.PanelBorder panelBorder1;
-	    private JButton btnNewButton;
+    	panel = new javax.swing.JLayeredPane();
+    	setBackground(new java.awt.Color(242, 242, 242));
+        panel.setLayout(new java.awt.GridLayout(1, 0, 10, 0));
+        
+        panelBorder1 = new view.swing.PanelBorder();
+        panelBorder1.setBackground(new java.awt.Color(255, 255, 255));
+        JScrollPane scroll = new JScrollPane(panelBorder1);
+        
+        //호텔INFO 부분
+        INFOIMG= new JPanel() {
+        	Image background = new ImageIcon(getClass().getResource("/view/icon/호텔썸네일.jpg")).getImage();
+        	public void paint(Graphics g) {
+        		g.drawImage(background, 0, 0, null);
+        	}
+        };
+        INFOIMG.setLayout(null);
+        INFOIMG.setVisible(true);
+        InfoLabel = new JLabel("<html><body><center>국내 최고의 럭셔리 비즈니스 호텔인 이화호텔 서울은 <br> 최고급 서비스를 제공하며 서울 아현동에 자리잡고 있습니다. <br> 1886년도에 설립된 전통있는 호텔로, 이화의 정신이 담겨있습니다.</center></body></html>");
+        InfoLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        //ROOMS&SUITE 부분
+        JPanel RSIMG = new JPanel() {
+        	Image background = new ImageIcon(getClass().getResource("/view/icon/조식.png")).getImage();
+        	public void paint(Graphics g) {
+        		g.drawImage(background, 0, 0, null);
+        	}
+        };
+        RSIMG.setLayout(null);
+        RSIMG.setVisible(true);
+        JLabel RSLabel = new JLabel();
+        RSLabel.setText("ROOMS&SUITE");
+        RSLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        RSLabel.setForeground(new Color(127, 127, 127));
+        RSLabel.setFont(new Font("SansSerif", Font.BOLD, 18));
+        RSLabel_1 = new JLabel("고객님의 여유로운 호텔스테이를 위해 조식서비스를 제공해드립니다");
+        RSLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        
+        
+        
+        javax.swing.GroupLayout panelBorder1Layout = new javax.swing.GroupLayout(panelBorder1);
+        panelBorder1Layout.setHorizontalGroup(
+        	panelBorder1Layout.createParallelGroup(Alignment.LEADING)
+        		.addGroup(panelBorder1Layout.createSequentialGroup()
+        			.addContainerGap()
+        			.addGroup(panelBorder1Layout.createParallelGroup(Alignment.LEADING)
+        				.addComponent(INFOIMG, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        				.addGroup(Alignment.TRAILING, panelBorder1Layout.createSequentialGroup()
+        					.addComponent(RSIMG, GroupLayout.DEFAULT_SIZE, 438, Short.MAX_VALUE)
+        					.addPreferredGap(ComponentPlacement.RELATED)
+        					.addComponent(RSLabel_1, GroupLayout.PREFERRED_SIZE, 420, GroupLayout.PREFERRED_SIZE))
+        				.addComponent(InfoLabel, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 863, Short.MAX_VALUE)
+        				.addComponent(RSLabel, GroupLayout.DEFAULT_SIZE, 863, Short.MAX_VALUE))
+        			
+        			.addContainerGap())
+        );
+        panelBorder1Layout.setVerticalGroup(
+        	panelBorder1Layout.createParallelGroup(Alignment.TRAILING)
+        		.addGroup(panelBorder1Layout.createSequentialGroup()
+        			.addContainerGap()
+        			.addComponent(INFOIMG, GroupLayout.PREFERRED_SIZE, 243, GroupLayout.PREFERRED_SIZE)
+        			.addPreferredGap(ComponentPlacement.UNRELATED)
+        			.addComponent(InfoLabel, GroupLayout.PREFERRED_SIZE, 53, GroupLayout.PREFERRED_SIZE)
+        			.addGap(30)
+        			.addComponent(RSLabel, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE)
+        			.addPreferredGap(ComponentPlacement.UNRELATED)
+        			.addGroup(panelBorder1Layout.createParallelGroup(Alignment.TRAILING)
+        				.addComponent(RSLabel_1, GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE)
+        				.addComponent(RSIMG, GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE))
+        			
+        			
+        			.addContainerGap())
+        );
+        panelBorder1.setLayout(panelBorder1Layout);
+        
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        layout.setHorizontalGroup(
+        	layout.createParallelGroup(Alignment.LEADING)
+        		.addGroup(layout.createSequentialGroup()
+        			.addGap(20)
+        			.addComponent(panel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+        			.addPreferredGap(ComponentPlacement.RELATED)
+        			.addComponent(panelBorder1, GroupLayout.PREFERRED_SIZE, 860, Short.MAX_VALUE)
+        			.addContainerGap())
+        );
+        layout.setVerticalGroup(
+        	layout.createParallelGroup(Alignment.LEADING)
+        		.addGroup(layout.createSequentialGroup()
+        			.addGap(20)
+        			.addComponent(panel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+        			.addContainerGap(20, Short.MAX_VALUE))
+        		.addGroup(layout.createSequentialGroup()
+        			.addContainerGap()
+        			.addComponent(panelBorder1, GroupLayout.DEFAULT_SIZE, 500, Short.MAX_VALUE))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+    private javax.swing.JLayeredPane panel;
+    private view.swing.PanelBorder panelBorder1;
+    private JLabel InfoLabel;
+    private JLabel RSLabel_1;
+    private JPanel INFOIMG;
 }
