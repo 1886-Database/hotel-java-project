ce5bc08ee57b86e87a6696ef8e82e6dde48a2b69
YuRi Chung <68888172+erica00j@users.noreply.github.com>
2023-06-04 21:32:42 +0900

    Update ReservationManager.java

--- hotel-project/src/modelManager/ReservationManager.java
+++ hotel-project/src/modelManager/ReservationManager.java
@@ -7,6 +7,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import myConnection.MY_Connection;
+import view.form.Form_Reservation;
+import view.form.Form_RoomReservation;
 
 public class ReservationManager {
 	private MY_Connection myConnection;
@@ -14,6 +16,8 @@ public class ReservationManager {
 	private Statement stmt;
 	private PreparedStatement ps;
 	private ResultSet rs;
+	private Form_Reservation form3;
+	private Form_RoomReservation rr;
 	
 	//기본 생성자 : 커넥션 객체 생성
 	public ReservationManager() {
@@ -23,27 +27,45 @@ public class ReservationManager {
 		catch(Exception e) {
 			e.printStackTrace();
 		}
+		form3 = new Form_Reservation();
+		rr = new Form_RoomReservation();
 	}
 	//입력받은 정보를 reservation 테이블에 insert
-		public int reservation(String ReservedNo,int RoomNo,String CheckIn,String CheckOut,String ReservedDate,int MemberID, String Requirement,Boolean Breakfast,String ReservedStatus,int GuestNo){
+		public int reservation(){
 			String SQL = "INSERT INTO DB2023_member VALUES(?,?,?,?,?,?,?,?,?,?)";
 			try {
 				con=myConnection.getConnection();
 				ps=con.prepareStatement(SQL);
 				stmt=con.createStatement();
-				rs=stmt.executeQuery("select max(memberID) from DB2023_member");
+				rs=stmt.executeQuery("select max(reservedNo) from DB2023_room");
 				while(rs.next()) {
-					int memberID = rs.getInt(1)+1;
-					ps.setString(1, ReservedNo);
-					ps.setInt(2, RoomNo);
-					ps.setString(3, CheckIn);
-					ps.setString(4, CheckOut);
-					ps.setString(5, ReservedDate);
-					ps.setInt(6, MemberID);
-					ps.setString(7, Requirement);
-					ps.setBoolean(8, Breakfast);
-					ps.setInt(9, GuestNo);
+					String[] s = form3.dataArr();
+					String MEMBERID = s[0];
+		            String ROOMNO = s[1];
+		            String CHECKIN = s[2];
+		            String CHECKOUT = s[3];
+		            
+		            String[] s2 = rr.dataArr2();
+		            String RESERVEDDATE = s2[0];
+		            String REQUIREMENT = s2[1];
+		            String BREAKFAST_ = s2[2];
+		            String RESERVEDSTATUS = s2[3];
+		            String GUESTNO = s2[4];
+		           
+					int reservedNo = rs.getInt(1)+1;
+					ps.setInt(1, reservedNo);
+					ps.setString(2, ROOMNO);
+					ps.setString(3, CHECKIN);
+					ps.setString(4, CHECKOUT);
+					ps.setString(5, RESERVEDDATE);
+					ps.setString(6, MEMBERID);
+					ps.setString(7, REQUIREMENT);
+					ps.setString(8, BREAKFAST_);
+					ps.setString(9, RESERVEDSTATUS);
+					ps.setString(10, GUESTNO);
 					ps.executeUpdate();
+					System.out.println("예약 완료"+" 예약 번호: "+reservedNo+" 예약 객실: "+ROOMNO+" 체크인: "+CHECKIN+" 체크아웃: "+CHECKOUT+" 예약 날짜: "+RESERVEDDATE+
+							" 멤버아이디: "+MEMBERID+" 요구사항: "+REQUIREMENT+" 조식여부: "+BREAKFAST_+" 상테 :"+RESERVEDSTATUS+" 인원: "+GUESTNO);
 					return 1;
 				}
 			}catch(SQLException se) {
