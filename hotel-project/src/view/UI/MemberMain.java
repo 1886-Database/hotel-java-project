4ff04212c9f1c8acba23388a605d88b1db977594
YuRi Chung <68888172+erica00j@users.noreply.github.com>
2023-06-06 02:09:12 +0900

    Update MemberMain.java

--- hotel-project/src/view/UI/MemberMain.java
+++ hotel-project/src/view/UI/MemberMain.java
@@ -45,7 +45,7 @@ public class MemberMain extends javax.swing.JFrame {
         loginID = id;
         setBackground(new Color(0, 0, 0, 0));
         home = new Form_Home();
-        form1 = new Form_RoomReservation();
+        
         form2 = new Form_EwhaHotel();
         form3 = new Form_Reservation();
         form4 = new Form_MyPage(loginID);
@@ -82,9 +82,10 @@ public class MemberMain extends javax.swing.JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                String[] s = form3.dataArr();
-               s[0] = String.valueOf(MemberID);
-               for(int i=1; i<s.length; ++i)
-                  System.out.println(s[i]);
+               
+               for(int i=0; i<s.length; ++i)
+                   System.out.println(s[i]);
+               form1 = new Form_RoomReservation(s);
                setForm(form1);
             }
          });
