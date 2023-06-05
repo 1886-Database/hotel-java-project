6920276783175c71f4d5eb1c74fc5640ee27532f
YuRi Chung <68888172+erica00j@users.noreply.github.com>
2023-06-06 02:08:47 +0900

    Update ReservationManager.java

--- hotel-project/src/modelManager/ReservationManager.java
+++ hotel-project/src/modelManager/ReservationManager.java
@@ -6,8 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import model.Employee;
-import model.Reservation;
 import myConnection.MY_Connection;
 import view.form.Form_Reservation;
 import view.form.Form_RoomReservation;
@@ -32,7 +30,7 @@ public class ReservationManager {
 		
 	}
 	//입력받은 정보를 reservation 테이블에 insert
-		public int reservation(){
+		public int reservation(String roomno, String checkin, String checkout, String reserveddate, String requirement, String breakfast, String reservedstatus, String guestno){
 			String SQL = "INSERT INTO DB2023_reservation VALUES(?,?,?,?,?,?,?,?,?,?)";
 			try {
 				con=myConnection.getConnection();
@@ -40,20 +38,30 @@ public class ReservationManager {
 				stmt=con.createStatement();
 				rs=stmt.executeQuery("select max(reservedNo) from DB2023_reservation");
 				while(rs.next()) {
-					form3 = new Form_Reservation();
-					String[] s1 = form3.dataArr();
-		            String ROOMNO = s1[0];
-		            String CHECKIN = s1[1];
-		            String CHECKOUT = s1[2];
-		            String MEMBERID = "00001";
+					//form3 = new Form_Reservation();
+					//String[] s1 = form3.dataArr();
+		            //String ROOMNO = s1[0];
+		            //String CHECKIN = s1[1];
+		            //String CHECKOUT = s1[2];
+		            //String MEMBERID = "1";
+					//rr = new Form_RoomReservation();
+		            //String[] s2 = rr.dataArr2();
+		            //String RESERVEDDATE = s2[0];
+		            //String REQUIREMENT = s2[1];
+		            //String BREAKFAST_ = s2[2];
+		            //String RESERVEDSTATUS = s2[3];
+		            //String GUESTNO = s2[4];
+					
+		            String ROOMNO = roomno;
+		            String CHECKIN = checkin;
+		            String CHECKOUT = checkout;
+		            String RESERVEDDATE = reserveddate;
+		            String MEMBERID = "1";
+		            String REQUIREMENT = requirement;
+		            String BREAKFAST_ = breakfast;
+		            String RESERVEDSTATUS = reservedstatus;
+		            String GUESTNO = guestno;
 		            
-		            rr = new Form_RoomReservation();
-		            String[] s2 = rr.dataArr2();
-		            String RESERVEDDATE = s2[0];
-		            String REQUIREMENT = s2[1];
-		            String BREAKFAST_ = s2[2];
-		            String RESERVEDSTATUS = s2[3];
-		            String GUESTNO = s2[4];
 		           
 					int reservedNo = rs.getInt(1)+1;
 					ps.setInt(1, reservedNo);
@@ -78,56 +86,4 @@ public class ReservationManager {
 			}
 			return -1; //데이터베이스 오류
 		}	
-
-	
-public Reservation[] getAllReservation() {
-	Reservation[] r_array;
-	int rowCnt=0;
-	int index=0;
-	String res = "SELECT * FROM DB2023_reservation";
-	String resCnt = "SELECT COUNT(*) FROM DB2023_reservation";
-	try {
-		con=myConnection.getConnection();
-		stmt=con.createStatement();
-		
-		//reservation 테이블의 레코드 개수 크기 만큼의 객체배열 생성
-		ResultSet rsCnt=stmt.executeQuery(resCnt);
-		
-		if(rsCnt.next()) {
-			rowCnt =rsCnt.getInt(1);
-		}
-		r_array=new Reservation[rowCnt];
-		for (int i = 0; i < r_array.length; ++i)
-			r_array[i] = new Reservation();
-		
-		rsCnt.close();     //자원반납
-		
-		//각 객체에 값 저장
-		rs=stmt.executeQuery(res);
-		while(rs.next()) {
-			r_array[index].setReservedNo(rs.getInt(1));
-			r_array[index].setRoomNo(rs.getInt(2));
-			r_array[index].setCheckIn(rs.getString(3));
-			r_array[index].setCheckOut(rs.getString(4));
-			r_array[index].setReservedDate(rs.getString(5));
-			r_array[index].setMemberID(rs.getInt(6));
-			r_array[index].setRequirement(rs.getString(7));
-			r_array[index].setBreakfast(rs.getBoolean(8));
-			r_array[index].setReservedStatus(rs.getString(9));
-			r_array[index].setGusetNo(rs.getInt(10));
-			
-			index++;
-		}
-	
-		return r_array;
-		
-	}catch(SQLException e) {
-		e.printStackTrace();
-	}finally {
-		myConnection.close(rs, stmt, null, con);
-	}
-	
-	r_array=new Reservation[rowCnt];
-	return r_array;
-}
 }
