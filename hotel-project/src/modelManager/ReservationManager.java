1f6e94bcfcfe4ee6de6037991d9a1895afd4cd0f
innoxo <97402987+innoxo@users.noreply.github.com>
2023-06-10 15:16:57 +0900

    view쿼리 수정

--- hotel-project/src/modelManager/ReservationManager.java
+++ hotel-project/src/modelManager/ReservationManager.java
@@ -86,24 +86,15 @@ public class ReservationManager {
 		Reservation[] r_array;
 		int rowCnt = 0;
 		int index = 0;
-		String res = "CREATE OR REPLACE VIEW DB2023_MemberReservationView AS "
-				+ "SELECT member.name, member.phone, reservation.reservedNo, reservation.roomNo, reservation.checkIn, reservation.checkOut, "
-				+ "reservation.reservedDate, reservation.memberID, reservation.reservedStatus, reservation.requirement "
-				+ "FROM DB2023_member AS member "
-				+ "JOIN DB2023_reservation AS reservation ON member.memberID = reservation.memberID;";
-		String resCnt = "SELECT COUNT(*) FROM DB2023_reservation";
+		String query = "SELECT name, phone, reservedNo, roomNo, checkIn, checkOut, reservedDate, memberID, reservedStatus, requirement "
+				+ "FROM DB2023_MemberReservationView";
+		String resCnt = "SELECT COUNT(*) FROM DB2023_MemberReservationView";
 		try {
 			con = myConnection.getConnection();
 			stmt = con.createStatement();
 
-			// 뷰가 이미 존재하는지 확인
-			String dropView = "DROP VIEW IF EXISTS DB2023_MemberReservationView";
-			stmt.executeUpdate(dropView);
-
-			// MemberReservaionView 뷰 생성
-			stmt.executeUpdate(res);
-
-			// reservation 테이블의 레코드 개수 확인
+	
+			// reservation 의 레코드 개수 확인
 			ResultSet rsCnt = stmt.executeQuery(resCnt);
 
 			if (rsCnt.next()) {
@@ -116,8 +107,7 @@ public class ReservationManager {
 			rsCnt.close(); // 자원 반환
 
 			// MemberReservationView 뷰에서 데이터 조회
-			String query = "SELECT name, phone, reservedNo, roomNo, checkIn, checkOut, reservedDate, memberID, reservedStatus, requirement "
-					+ "FROM DB2023_MemberReservationView";
+	
 			rs = stmt.executeQuery(query);
 			while (rs.next()) {
 				r_array[index].setMemberName(rs.getString(1));
